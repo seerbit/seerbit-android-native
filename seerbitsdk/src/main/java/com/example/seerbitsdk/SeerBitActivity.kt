c1223a8319f18ce0dfea9c1d7e00e3aef04ed953
Adeife Taiwo <adeifetaiwo50@gmail.com>
2023-02-08 10:56:17 +0100

    fix navigation issue

--- seerbitsdk/src/main/java/com/example/seerbitsdk/SeerBitActivity.kt
+++ seerbitsdk/src/main/java/com/example/seerbitsdk/SeerBitActivity.kt
@@ -6,14 +6,12 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
 import androidx.compose.runtime.*
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
