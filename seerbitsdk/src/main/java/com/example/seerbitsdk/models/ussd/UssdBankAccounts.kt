7c4d2248100c5801b5764463a7a392551e6e7498
Adeife Taiwo <adeifetaiwo50@gmail.com>
2023-03-23 12:44:13 +0100

    updated bank bvn details

--- seerbitsdk/src/main/java/com/example/seerbitsdk/models/ussd/UssdBankAccounts.kt
+++ seerbitsdk/src/main/java/com/example/seerbitsdk/models/ussd/UssdBankAccounts.kt
@@ -12,94 +12,101 @@ object UssdBankData {
         UssdBankAccounts(
             "044",
             "ACCESS BANK PLC",
-            "ACCESS BANK's"),
+            "ACCESS BANK's"
+        ),
 
         UssdBankAccounts(
             "063",
             "ACCESS DIAMOND PLC",
-            "ACCESS DIAMOND's"),
+            "ACCESS DIAMOND's"
+        ),
 
         UssdBankAccounts(
             "050",
             "ECOBANK BANK PLC",
-            "ECOBANK's"),
+            "ECOBANK's"
+        ),
 
         UssdBankAccounts(
             "070",
             "FIDELITY BANK PLC",
-            "FIDELITY BANK's"),
+            "FIDELITY BANK's"
+        ),
 
         UssdBankAccounts(
             "011",
             "FIRST BANK OF NIGERIA PLC",
-            "FIRST BANK's"),
+            "FIRST BANK's"
+        ),
 
         UssdBankAccounts(
             "214",
             "FIRST CITY MONUMENT BANK PLC",
-            "ACCESS BANK's"),
+            "FCMB"
+        ),
 
-        UssdBankAccounts(
-            "044",
-            "ACCESS BANK PLC",
-            "FCMB's"),
-
-        UssdBankAccounts(
-            "044",
-            "ACCESS BANK PLC",
-            "ACCESS BANK's"),
 
         UssdBankAccounts(
             "058",
             "Guarantee Trust Bank PLC",
-            "GTBank's"),
+            "GTBank's"
+        ),
 
         UssdBankAccounts(
             "082",
             "KEYSTONE BANK",
-            "KEYSTONE BANK's"),
+            "KEYSTONE BANK's"
+        ),
 
         UssdBankAccounts(
             "090175",
             "HIGHSTREET MICROFINANCE BANK",
-            "HIGHSTREET's"),
+            "HIGHSTREET's"
+        ),
 
         UssdBankAccounts(
             "221",
             "STANBIC IBTC BANK PLC",
-            "STANBIC IBTC's"),
+            "STANBIC IBTC's"
+        ),
 
         UssdBankAccounts(
             "032",
             "UNION BANK OF NIGERIA PLC",
-            "UNION BANK's"),
+            "UNION BANK's"
+        ),
 
         UssdBankAccounts(
             "215",
             "UNITY BANK PLC",
-            "UNITY BANK's"),
+            "UNITY BANK's"
+        ),
 
         UssdBankAccounts(
             "090110",
             "VFD MICROFINANCE BANK",
-            "VFD's"),
+            "VFD's"
+        ),
 
         UssdBankAccounts(
             "035",
             "WEMA BANK PLC",
-            "WEMA BANK's"),
+            "WEMA BANK's"
+        ),
 
         UssdBankAccounts(
             "057",
             "ZENITH BANK PLC",
-            "ZENITH BANK's"),
+            "ZENITH BANK's"
+        ),
 
         UssdBankAccounts(
             "322",
             "Zenith Mobile",
-            "Zenith Mobile's"),
+            "Zenith Mobile's"
+        ),
 
 
-    )
+        )
 
 }
